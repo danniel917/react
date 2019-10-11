1565f1f0f262732ce6062996b275fd9189856baa
danniel917 <miemail@dominio.com>
2019-10-11 18:04:33 -0500

    Codigo realizado en la clase 3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import "./styles/styles.scss";
 import Curso from "./curso";
 
+const cursos = [
+  { "title": "React desde cero",  "image": "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png", "price": 40, "profesor": "Daniel Morales"},
+  { "title": "Drupal desde cero",  "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/drupal-poster-720_3.jpg?itok=e93ErhMN", "price": 50, "profesor": "Andres Cea"},
+  { "title": "Go desde cero",  "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/go_0.jpg?itok=k2amLhrN", "price": 20, "profesor": "Nelson Rivera"},
+  { "title": "HTML desde cero",  "image": "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-10/HTML%202020.png", "price": 0, "profesor": "Julio Jaramillo"}
+]
+
 const App = () => (
   <div className="ed-grid m-grid-3">
-    <Curso />
-    <Curso />
-    <Curso />
+    {
+      cursos.map( c => <Curso title={c.title} image={c.image} price={c.price} profesor={c.profesor} />)
+    }
   </div>
 
 
--- src/curso.js
+++ src/curso.js
@@ -1,37 +1,50 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Curso = () => (
-  <article class="card">
-    <div class="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img
-        src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-        alt=""
-      />
+const Curso = ({ title, image, price, profesor }) => (
+  <article className="card">
+    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+      <img src={image} alt={title} />
     </div>
-    <div class="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 class="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-      </h3>
-      <div class="s-mb-2 s-main-center">
-        <div class="card__teacher s-cross-center">
-          <div class="card__avatar s-mr-1">
-            <div class="circle img-container">
+    <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+      <h3 className="t5 s-mb-2 s-center">{title}</h3>
+      <div className="s-mb-2 s-main-center">
+        <div className="card__teacher s-cross-center">
+          <div className="card__avatar s-mr-1">
+            <div className="circle img-container">
               <img
                 src="https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt=""
+                alt="sdfs"
               />
             </div>
           </div>
-          <span class="small">Alexys Lozada</span>
+          <span className="small">{profesor}</span>
         </div>
       </div>
-      <div class="s-main-center">
-        <a class="button--ghost-alert button--tiny" href="#">
-          $ 20USD
+      <div className="s-main-center">
+        <a className="button--ghost-alert button--tiny" href="#">
+          {`$ ${price} USD`}
         </a>
       </div>
     </div>
   </article>
 );
+
+// Tipo de propiedad que se espera
+Curso.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.string,
+  profesor: PropTypes.string
+};
+
+// Propiedades por Default
+Curso.defaultProps = {
+  title: "No se encontró titulo",
+  image:
+    "https://cdn3.wpbeginner.com/wp-content/uploads/2018/03/httperrorwpuploads-1.png",
+  price: "--",
+  profesor: " "
+};
 
 export default Curso;
